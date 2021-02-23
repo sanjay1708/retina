14459783002397e5f33dfc2ac8c4474187520902
sanjay1708 <54763695+sanjay1708@users.noreply.github.com>
2021-02-23 18:15:22 +0530

    Timer module implemented using thread

--- timer.py
+++ timer.py
@@ -1,18 +1,24 @@
-# importing the time module 
 import time 
-  
-# define the countdown func. 
-def countDown(t): 
-    while t: 
-        mins, secs = divmod(t, 60) 
-        timer = '{:02d}:{:02d}'.format(mins, secs) 
-        print(timer, end="\r") 
-        time.sleep(1) 
-        t -= 1
-      
-    return True 
+from threading import *
+from playsound import playsound
 
-  
 
-  
+class timer(Thread):
 
+    def countDown(self,t): 
+	 
+	    while t: 
+		    mins, secs = divmod(t, 60) 
+		    timer = '{:02d}:{:02d}'.format(mins, secs) 
+		    print(timer, end="\r") 
+		    time.sleep(1) 
+		    t -= 1
+
+    def run(self):
+        self.countDown(12)
+        playsound('audio/lookaway.mp3')
+        self.countDown(20)
+        playsound('audio/resume.mp3')
+
+
+ 
